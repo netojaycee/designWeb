0612d3bc95e8f1a9c8e967b861b7b6adc8d2c867
John Chinonso Edeh <netojaycee@gmail.com>
2024-06-04 00:26:26 +0100

    added lower navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import NavBottom from "./NavBottom";
 
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
         <Header />
         <Outlet />
         <Footer />
+        <NavBottom />
       </div>
     </>
   );
